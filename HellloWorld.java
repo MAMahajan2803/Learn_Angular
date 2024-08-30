bf79c6ae9d90b63e8b767dc04eec99f5075af874
MAMahajan2803 <mrudulamahajan28@gmail.com>
2024-08-30 22:36:26 +0530

    Update HellloWorld.java

--- HellloWorld.java
+++ HellloWorld.java
@@ -3,5 +3,6 @@ public class HellloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World");
         System.out.println("I am learning angular");
+        System.out.println("This is for checking auto build");
     }
 }
